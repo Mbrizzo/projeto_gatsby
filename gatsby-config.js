01327d5095802439e2aa71233e16bd1992568d76
MRizzo <mrizzo25@gmail.com>
2020-12-01 17:03:19 -0300

    cleanup start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `FrontEnd`,
+    description: `The blog of the....`,
+    author: `@mrizzo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
