9747561e06bbe27517bfc4c0ff812bb28fe8b8da
MRizzo <mrizzo25@gmail.com>
2020-12-02 01:32:19 -0300

    added and stylized Sidebar component

--- src/styles/global.js
+++ src/styles/global.js
@@ -50,7 +50,7 @@ table {
 	border-spacing: 0;
 }
 body {
-  background: #16202c;
+  background: #353535;
   line-height: 1;
   font-size: 100%;
   font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
