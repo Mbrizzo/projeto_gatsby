6443e0684a3a27ce095675ff0df1e8e3fa9be479
MRizzo <mrizzo25@gmail.com>
2020-12-03 21:50:50 -0300

    add menu-right

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react"
+
+import { Home } from "@styled-icons/boxicons-solid/Home"
+import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
+import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
+import { Bulb as Light } from "@styled-icons/boxicons-regular/Bulb"
+import { Grid } from "@styled-icons/boxicons-solid/Grid"
+
+import * as S from "./styled"
 
 const MenuBar = () => (
-
+  <S.MenuBarWrapper>
+    <S.MenuBarGroup>
+      <S.MenuBarLink to="/" title="Home">
+        <S.MenuBarItem>
+          <Home />
+        </S.MenuBarItem>
+      </S.MenuBarLink>
+      <S.MenuBarLink to="/search" title="Pesquisar">
+        <S.MenuBarItem>
+          <Search />
+        </S.MenuBarItem>
+      </S.MenuBarLink>
+    </S.MenuBarGroup>
+    <S.MenuBarGroup>
+      <S.MenuBarItem title="Mudar Theme">
+        <Light />
+      </S.MenuBarItem>
+      <S.MenuBarItem title="Mudar Visualização">
+        <Grid />
+      </S.MenuBarItem>
+      <S.MenuBarItem title="Voltar para o Topo">
+        <Arrow />
+      </S.MenuBarItem>
+    </S.MenuBarGroup>
+  </S.MenuBarWrapper>
 )
 
-export default MenuBar;
+export default MenuBar
